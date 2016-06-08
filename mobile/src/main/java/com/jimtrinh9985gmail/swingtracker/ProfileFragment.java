b4c2b0656664718a63ee6ccdee71a9a4da576a00
Kimo <jimtrinh9985@gmail.com>
2016-06-08 03:26:01 -0700

    feat:  Create Image Profile

--- mobile/src/main/java/com/jimtrinh9985gmail/swingtracker/ProfileFragment.java
+++ mobile/src/main/java/com/jimtrinh9985gmail/swingtracker/ProfileFragment.java
@@ -1,24 +1,17 @@
 package com.jimtrinh9985gmail.swingtracker;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.squareup.picasso.MemoryPolicy;
-import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
-import java.io.File;
 
 /**
  * Created by Kimo on 6/6/2016.
@@ -29,6 +22,9 @@ public class ProfileFragment extends AppCompatActivity {
 
     private static int LOAD_IMAGE_RESULTS = 1;
     private ImageView profileImage;
+    private TextView profileName, profileHeight, profileWeight, profileGrip, profileRacket;
+    private String pName, pHeight, pWeight, pGrip, pRacket;
+    private String imageUri;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,10 +32,17 @@ public class ProfileFragment extends AppCompatActivity {
         setContentView(R.layout.profile_fragment);
 
         profileImage = (ImageView) findViewById(R.id.photo);
+        profileName = (TextView) findViewById(R.id.profile_name);
+        profileHeight = (TextView) findViewById(R.id.profile_height);
+        profileWeight = (TextView) findViewById(R.id.profile_weight);
+        profileGrip = (TextView) findViewById(R.id.profile_grip);
+        profileRacket = (TextView) findViewById(R.id.profile_racket);
+        loadPlayerProfile();
 
         profileImage.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
+                // Choose profile image from Gallery //
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.
                         Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, LOAD_IMAGE_RESULTS);
@@ -53,22 +56,31 @@ public class ProfileFragment extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == LOAD_IMAGE_RESULTS && resultCode == Activity.RESULT_OK && data != null) {
-            String image = data.getData().toString();
+            imageUri = data.getData().toString();
+
+            UtilityProfile.saveImageUri(this, imageUri);
+
             Picasso.with(this)
-                    .load(image)
+                    .load(imageUri)
                     .noPlaceholder().centerCrop().fit()
                     .error(R.drawable.player1)
                     .into(profileImage);
         }
     }
 
-    private void loadPlayerPicture() {
+    private void loadPlayerProfile() {
 
+        imageUri = UtilityProfile.getPrefImageUri(this);
+        Picasso.with(this)
+                .load(imageUri)
+                .noPlaceholder().centerCrop().fit()
+                .error(R.drawable.player1)
+                .into(profileImage);
+
+        pName = UtilityProfile.getPrefProfileName(this);
+        pHeight = UtilityProfile.getPrefProfileHeight(this);
+        pWeight = UtilityProfile.getPrefProfileWeight(this);
+        pGrip = UtilityProfile.getPrefProfileGrip1(this);
+        pRacket = UtilityProfile.getPrefProfileRacket(this);
     }
-
-    // Insert picture from gallery and save as profile //
-    private void insertSavePlayerPicture() {
-
-    }
-
 }
---
+++ mobile/src/main/java/com/jimtrinh9985gmail/swingtracker/UtilityProfile.java
@@ -0,0 +1,91 @@
+package com.jimtrinh9985gmail.swingtracker;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Kimo on 6/8/2016.
+ */
+public class UtilityProfile {
+
+    public final String LOG_TAG = UtilityProfile.class.getSimpleName();
+
+    public static final String SP_KEY_IMAGE = "com.swingtracker.IMAGE";
+    public static final String SP_KEY_NAME = "com.swingtracker.NAME";
+    public static final String SP_KEY_HEIGHT = "com.swingtracker.HEIGHT";
+    public static final String SP_KEY_WEIGHT = "com.swingtracker.WEIGHT";
+    public static final String SP_KEY_GRIP1 = "com.swingtracker.GRIP1";
+    public static final String SP_KEY_RACKET = "com.swingtracker.RACKET";
+
+    public static void saveImageUri(Context context, String imageUri) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.IMAGE",
+                Context.MODE_PRIVATE);
+        prefs.edit().putString(SP_KEY_IMAGE, imageUri).apply();
+    }
+
+    public static String getPrefImageUri(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.IMAGE",
+                Context.MODE_PRIVATE);
+        return prefs.getString(SP_KEY_IMAGE, "null");
+    }
+
+    public static void savePrefProfileName(Context context, String profileName) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.NAME",
+                Context.MODE_PRIVATE);
+        prefs.edit().putString(SP_KEY_NAME, profileName).apply();
+    }
+
+    public static String getPrefProfileName(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.NAME",
+                Context.MODE_PRIVATE);
+        return prefs.getString(SP_KEY_NAME, "Name: ");
+    }
+
+    public static void savePrefProfileHeight(Context context, String profileHeight) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.HEIGHT",
+                Context.MODE_PRIVATE);
+        prefs.edit().putString(SP_KEY_HEIGHT, profileHeight).apply();
+    }
+
+    public static String getPrefProfileHeight(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.HEIGHT",
+                Context.MODE_PRIVATE);
+        return prefs.getString(SP_KEY_HEIGHT, "Height: ");
+    }
+
+    public static void savePrefProfileWeight(Context context, String profileWeight) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.WEIGHT",
+                Context.MODE_PRIVATE);
+        prefs.edit().putString(SP_KEY_WEIGHT, profileWeight).apply();
+    }
+
+    public static String getPrefProfileWeight(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.WEIGHT",
+                Context.MODE_PRIVATE);
+        return prefs.getString(SP_KEY_WEIGHT, "Weight: ");
+    }
+
+    public static void savePrefProfileGrip1(Context context, String profileGrip1) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.GRIP1",
+                Context.MODE_PRIVATE);
+        prefs.edit().putString(SP_KEY_GRIP1, profileGrip1).apply();
+    }
+
+    public static String getPrefProfileGrip1(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.GRIP1",
+                Context.MODE_PRIVATE);
+        return prefs.getString(SP_KEY_GRIP1, "Grip: ");
+    }
+
+    public static void savePrefProfileRacket(Context context, String profileRacket) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.RACKET",
+                Context.MODE_PRIVATE);
+        prefs.edit().putString(SP_KEY_RACKET, profileRacket).apply();
+    }
+
+    public static String getPrefProfileRacket(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("com.swingtracker.RACKET",
+                Context.MODE_PRIVATE);
+        return prefs.getString(SP_KEY_RACKET, "Racket: ");
+    }
+}
